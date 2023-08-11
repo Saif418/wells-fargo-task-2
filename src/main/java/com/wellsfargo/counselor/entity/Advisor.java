9019d16cce95d7e4e8bfb218fa96be65ab6d158a
Saif Khan <sai.mk1009@gmail.com>
2023-08-11 15:46:03 +0300

    New corrected changes

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,5 +1,6 @@
 package com.wellsfargo.counselor.entity;
-package com.javax.persistence;
+
+import jakarta.persistence.*;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -195,41 +196,134 @@ public class Client {
 public class Portfolio {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long PortfolioId;
+    @GeneratedValue()
+    private long portfolioId;
+
+    @ManyToOne
+    private Client client;
 
     @Column(nullable = false)
-    private Date creationdate = new Date;
-
+    private String creationDate;
 
     protected Portfolio() {
-        public Portfolio(long PortfolioId, Date creationdate)
-        {
-            this.creationdate = creationdate;
-            this.PortfolioId = PortfolioId;
-        }
+
     }
-    public String toString()
-    {
-        return String.format(
-                "Portfolio[id=%d, creationdate='%s']",
-                id, creationdate);
-        )
-    }
-    public Portfolio(long PortfolioId, Date creationdate) {
-        this.PortfolioId = PortfolioId;
-        this.creationdate = creationdate;
+
+    public Portfolio(Client client, String creationDate) {
+        this.client = client;
+        this.creationDate = creationDate;
     }
 
     public Long getPortfolioId() {
-        return PortfolioId;
+        return portfolioId;
     }
 
-    public Date getCreationdate() {
-        return creationdate;
+    public Client getCLient() {
+        return client;
     }
 
+    public void setClient(Client client) {
+        this.client = client;
+    }
 
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
 }
 
-//
+// for Security
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @ManyToOne
+    private Portfolio portfolio;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private float purchasePrice;
+
+    @Column(nullable = false)
+    private String purchaseDate;
+
+    @Column(nullable = false)
+    private float quantity;
+
+    protected Security() {
+
+    }
+
+    public Security(Portfolio portfolio, String name, String category, float purchasePrice, String purchaseDate, float quantity) {
+        this.portfolio = portfolio;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+}
